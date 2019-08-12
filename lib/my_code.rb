72bb78ed76df79020f80a674e48f29f3b9d0ddb4
K Hong <albtrossfacefall@gmail.com>
2019-08-12 20:15:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,8 @@ def map(source_array)
   i=0
   new_array=[]
   while i<source_array.length do
-    new_array[i]=source_array[i]
+    new_array = yield (source_array[i])
     i+=1  
   end
-  yield(source_array[i])
+  new_array
 end
