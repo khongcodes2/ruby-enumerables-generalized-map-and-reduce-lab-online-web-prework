4a66133c9db502a77e4e155b9236981bff0982e7
K Hong <albtrossfacefall@gmail.com>
2019-08-12 19:57:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,12 @@
 # Your Code Here
-def my own map(source_array)
-  yield(source_array)
+def my own map(source_array, factor=1)
+  i=0
+  eq = []
+  while i<source_array.length do
+    source_array.length[i]*factor
+  end
+  yield
 end
 
-my own map(map)
-  {|array| 
+my own map(source_array, -1)
+  {|factor| 
